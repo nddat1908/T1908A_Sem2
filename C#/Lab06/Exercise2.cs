ab20b00ae8e8b596ebedf319c63f74b95f640a01
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2020-05-21 20:11:21 +0700

    Lab06

---
+++ C#/Lab06/Exercise2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    public class InvalidInput : ApplicationException
+    {
+        public InvalidInput():base("Enter less than 60,000 salary") { }
+        
+    }
+    public class InvalidBonus : ApplicationException
+    {
+        public InvalidBonus() : base("Enter more than 10,000") { }
+    }
+    class TestExcep
+    {
+        public static void Main()
+        {
+            double Salary;
+            int intSalary,bonusSalary = 0;
+            Console.WriteLine("Enter salary :");
+            try
+            {
+                intSalary = Convert.ToInt32(Console.ReadLine());
+                if(intSalary >= 60000)
+                {
+                    throw new InvalidInput();
+                }
+            }
+            catch (InvalidInput objInvalidInput)
+            {
+                Console.WriteLine(objInvalidInput.Message);
+            }
+            catch (System.FormatException objFormatException)
+            {
+                Console.WriteLine(objFormatException.Message);
+            }
+            Console.WriteLine("Enter bonusSalary :");
+            try
+            {
+                bonusSalary = Convert.ToInt32(Console.ReadLine());
+                if (bonusSalary >= 10000)
+                {
+                    throw new InvalidBonus();
+                }
+            }
+            catch (InvalidBonus objInvalidBonus)
+            {
+                Console.WriteLine(objInvalidBonus.Message);
+            }
+            catch (System.FormatException objFormatException)
+            {
+                Console.WriteLine(objFormatException.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
