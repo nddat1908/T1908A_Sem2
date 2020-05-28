cf5dcddba8fbe1842f7e1389dee2ac4dc7cce0f2
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2020-05-28 16:08:21 +0700

    Lab10

---
+++ C#/Lab10/PrintString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Delegate
+{
+    class PrintString
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        public delegate void printString(string s);
+
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine("The String is: {0}", str);
+        }
+        public static void WriteToFile(string s)
+        {
+            fs = new FileStream("c:\\message.txt",FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine();
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        public static void sendString(printString ps)
+        {
+            ps("Hello World");
+        }
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+            sendString(ps1);
+            sendString(ps2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Lab10/TestDelegate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+delegate int NumberChanger(int n);
+namespace DelegateApp1
+{
+    class TestDelegate
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+        static void Main(string[] args)
+        {
+            NumberChanger nc;
+            NumberChanger nc1 = new NumberChanger(AddNum);
+            NumberChanger nc2 = new NumberChanger(MultNum);
+
+            nc = nc1;
+            nc += nc2; 
+
+            nc(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+
+            /*nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();*/
+        }
+    }
+}
+    
