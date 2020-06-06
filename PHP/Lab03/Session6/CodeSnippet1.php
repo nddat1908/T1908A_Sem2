a957792a8786ee659145a36a9071a831ce77e349
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2020-06-06 16:48:46 +0700

    Lab03

---
+++ PHP/Lab03/Session6/CodeSnippet1.php
@@ -0,0 +1,12 @@
+
+<html>
+<head>
+    <title>String Assignment Opeation</title>
+</head>
+<body>
+<?php
+$string_val = "This is John Cizzel.";
+echo $string_val;
+?>
+</body>
+</html>
---
+++ PHP/Lab03/Session6/CodeSnippet6.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+    <title>Static Variables</title>
+</head>
+<body>
+<?php
+function decrement(){
+    static $static_int1 = 99;
+    $static_int1--;
+    echo "The decrement value is $static_int1";
+}
+decrement();
+?>
+</body>
+</html>
