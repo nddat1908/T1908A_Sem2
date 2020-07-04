fcf3857286427e8975dc6803ea0175bfcab2c98b
nddat1908 <54591227+nddat1908@users.noreply.github.com>
2020-07-04 18:54:44 +0700

    Laravel

--- Laravel/Gallery Project/web.php
+++ Laravel/Gallery Project/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('gallery',function (){
+/*Route::get('gallery',function (){
    return view('gallery');
-});
+});*/
+
+Route::get('/gallery','GalleryController@showGallery');
